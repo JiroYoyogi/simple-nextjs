f7e96e62750e037f4852edbf017000a49b7030be
JiroYoyogi <jiro.yoyogi.v5@gmail.com>
2025-07-27 11:21:47 +0900

    現在時刻を表示

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
+
+  const d = new Date().toISOString();
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -22,6 +25,9 @@ export default function Home() {
           </li>
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
+          </li>
+          <li className="tracking-[-.01em]">
+            {d}
           </li>
         </ol>
 
